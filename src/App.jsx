45fc3411afe094af4e28c8543dd5da5ce476f8b3
federicosimone <federico.simone15@gmail.com>
2026-01-17 13:07:43 +0100

    sistemazione + commenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-
-const languages = [
-  {
-    id: 1,
-    title: "HTML",
-    description: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png",
-  },
-  {
-    id: 2,
-    title: "CSS",
-    description: "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web.",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
-  },
-  {
-    id: 3,
-    title: "JavaScript",
-    description: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web.",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s",
-  },
-  {
-    id: 4,
-    title: "Node.js",
-    description: "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server.",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png",
-  },
-  {
-    id: 5,
-    title: "Express",
-    description: "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste.",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/250px-Expressjs.png"
-  },
-  {
-    id: 6,
-    title: "ReactJS",
-    description: "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione.",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s"
-  }
-];
+import languages from "../react-use-state-assets/languages"
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(-1)
-
-
-
 
   return (
     <>
@@ -52,6 +11,7 @@ function App() {
       <nav className="pt-1 ps-2">
         <h1>Learn Web Development</h1>
       </nav>
+
       <div className="container mt-5">
         <div className="row">
           <div className="col">
@@ -61,14 +21,21 @@ function App() {
                 languages.map((language, index) => {
 
                   let selected = selectedIndex == index ? "yellow" : "";
+
                   return <li key={language.id}><button className={`btn btn-primary ${selected}`} onClick={() => setSelectedIndex(() => index)}>{language.title}</button></li>
+                  {/*in setSelectedIndex vado a scrivere, ovvero, QUANDO CLICCO, MI CAMBI IL VALORE DI selectedIndex, prendendo come valore index...che vado a prendere dal 
+                    ciclo map() che me lo passa come parametro. Quindi ogni volta che clicco, prendo l'indice dell'elemento cliccato.*/}
                 })
               }
+
             </ul>
+
+
           </div>
           <div className="col">
             <div className="card" style={{ width: '18rem' }}>
-              {selectedIndex >= 0 ?
+
+              {selectedIndex >= 0 ? /*attenzione al ternario ==> se la prima condizione è verificata allora svolgi il codice , altrimenti svolgi l'altro (dopo i due punti)*/
                 <div className="card-body">
                   <h2 className="card-title">
                     {languages[selectedIndex].title}
@@ -82,7 +49,8 @@ function App() {
               }
 
 
-
+              {/*MA QUINDI SI PUO UTILIZZARE UN COMPONENTE PER LA CARD E UN COMPONENTE PER IL BOTTONE, MA PASSANDO LE PROPS DAL PADRE? IN MODO CHE PRENDANO LE INFO DALLA PAGINA DEL PADRE
+              ATTENZIONE! DEVI FAR PASSARE ANCHE LA FUNZIONE COME PROP, PERCHE' AL COMPONENTE ESTERNO DEVO PASSARGLI ANCHE LA FUNZIONE (DEL PADRE) DA CHIAMARE.*/}
 
 
             </div>
